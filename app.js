d90602f0c44a02bc295959bd5989e4d75a2bad19
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2019-02-14 11:43:28 +0530

    Uploading the JavaScript file

---
+++ app.js
@@ -0,0 +1,14 @@
+var app=angular.module('myApp',[]);
+app.controller('myCtrl', function($scope){
+ 
+  $scope.firstName = "";
+  $scope.lastName = "";
+  $scope.compName = "";
+  $scope.desgName = "";
+  $scope.dojName = "";
+  $scope.stName = "";
+  $scope.ctName = "";
+  $scope.msgName = "";
+
+
+});
